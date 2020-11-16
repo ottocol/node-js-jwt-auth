1ea9e8efb80b811d0165807471112795c5eff027
Otto Colomina <otto@dccia.ua.es>
2020-11-16 19:29:24 +0100

    crear roles al inicio

--- server.js
+++ server.js
@@ -20,7 +20,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync();
+db.sequelize.sync().then(()=>{
+  Role.findOne().then((found)=>{
+    if (!found)
+      initial()
+  })
+  
+}) ;
+
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Database with { force: true }');
